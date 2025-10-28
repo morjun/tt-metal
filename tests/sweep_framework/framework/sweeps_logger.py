e892cafe64ba565726046ce040bfbe75ba85f1bb
morjun <junmo2011@gmail.com>
2025-10-28 14:07:37 +0900

    Update some tests

--- tests/sweep_framework/framework/sweeps_logger.py
+++ tests/sweep_framework/framework/sweeps_logger.py
@@ -26,4 +26,7 @@ logger.add(
 logger.add(
     sys.stderr, level="INFO", format=sweeps_format, filter=lambda record: record["extra"].get("name") == "sweeps"
 )
+logger.add(
+    "output.log", level="INFO", format=sweeps_format, filter=lambda record: record["extra"].get("name") == "sweeps"
+)
 sweeps_logger = logger.bind(name="sweeps")
